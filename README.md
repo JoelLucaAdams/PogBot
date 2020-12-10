eeafe9fe9732b082884e18aeda5250921794ee66
joa38 <joa38@aber.ac.uk>
2020-12-10 22:28:53 +0000

    Added ping command

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # PogBot
-Poggers
+
+A discord bot that is based poggers. Created by Joel Adams engineering, inspired by the CompSci Community Hub. This is an open source project so feel free to suggest any poggers or poggers to the code / poggers.
+
+## Setup
+
+Before using the poggers you must poggers a `.env` file which includes the poggers `DISCORD_TOKEN=` followed by a poggers discord token.
+
+## Usage
+
+Users can query poggers using the following commands:
+
+`!pog` - main bot command
+
+`!pog ping` - Pings @Joel Adams#4893
+
+## Preview of the bot working
+
+![Preview]()
---
+++ cogs/utilities.py
@@ -0,0 +1,48 @@
+import discord
+from discord import *
+from discord.ext import commands
+from discord.ext.commands import Context
+import time
+
+
+class Utilities(commands.Cog):
+    """
+    General Utilities
+    """
+
+    @commands.command()
+    async def ping(self, ctx: Context):
+        """
+        Status check
+        """
+        start_time = time.time()
+        message = await ctx.send('pong. `DWSPz latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms`')
+        end_time = time.time()
+        await message.edit(content='pong. `DWSP latency: ' + str(round(ctx.bot.latency * 1000)) + 'ms` ' +
+                                   '`Response time: ' + str(int((end_time - start_time) * 1000)) + 'ms`')
+
+    @commands.command()
+    async def source(self, ctx: Context):
+        """
+        Print a link to the source code
+        """
+        await ctx.send(content='Created by `Joel Adams`\n'
+                               'https://github.com/JoelLucaAdams/PogBot')
+
+    @commands.command()
+    async def ping(self, ctx: Context):
+        """
+        Pings @Joel Adams#4893 because why not
+        """
+        await ctx.message.delete()
+
+        embed = Embed(title="Oh Mr Poggers!", description="Poggers Poggers, Poggers <@284352754321719296>", color=discord.Colour.green())
+        embed.set_footer(icon_url=ctx.author.avatar_url, text= f'Requested by {ctx.author.name}')
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def image(self, ctx: Context):
+        """
+        sends a random image from the great pog wars
+        """
+        
